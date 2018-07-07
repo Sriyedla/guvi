c0d2dd23de9549abbda865eb44d6a2da3a93bd26
Sriyedla <39210384+Sriyedla@users.noreply.github.com>
2018-07-07 09:54:07 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,7 +1,8 @@
 a=int(raw_input("enter the number"))
-if(a%2==0):
-	print("even")
-elif(a%2==1):
-	print("odd")
+if a>=2 and a<=100000:
+ if(a%2==0):
+	 print("even")
+ else:
+	 print("odd")
 else:
 	print("invalid")
